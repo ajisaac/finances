c3543204176ecc3566b193bf462556d060eead94
Aaron Isaac <aaron@aisaac.co>
2024-01-17 16:49:26 -0800

    Parse files into db

--- src/main/java/co/aisaac/finances/MainController.java
+++ src/main/java/co/aisaac/finances/MainController.java
@@ -42,6 +42,67 @@ public class MainController {
 		return "index";
 	}
 
+	// todo, improve the input format
+
+	private List<FinancialTransaction> addCreditCardTransactions(List<CreditCardTransaction> creditCardTransactions, String name) {
+		List<FinancialTransaction> transactions = new ArrayList<>();
+		for (var tx : creditCardTransactions) {
+			FinancialTransaction ft = new FinancialTransaction();
+			ft.setAccountName(name);
+			ft.setDate(tx.getDate());
+			ft.setDescription(tx.getName());
+			if (tx.getAmount() > 0) {
+				if (!tx.getTransaction().equals("CREDIT"))
+					throw new IllegalStateException("Expected CREDIT, got " + tx.getTransaction());
+				ft.setCredit(BigDecimal.valueOf(tx.getAmount()));
+			} else {
+				if (!tx.getTransaction().equals("DEBIT"))
+					throw new IllegalStateException("Expected DEBIT, got " + tx.getTransaction());
+				ft.setDebit(BigDecimal.valueOf(Math.abs(tx.getAmount())));
+			}
+			transactions.add(ft);
+
+			// todo grab that memo at some point
+		}
+		return transactions;
+	}
+
+	private List<FinancialTransaction> addAcvTransactions(List<AcVeTransaction> abankTransactions, String name) {
+		List<FinancialTransaction> transactions = new ArrayList<>();
+		for (var tx : abankTransactions) {
+			FinancialTransaction ft = new FinancialTransaction();
+			ft.setAccountName(name);
+			ft.setDate(tx.getDate());
+			ft.setDescription(tx.getDescription());
+
+			if (StringUtils.isNotBlank(tx.getDebit())) {
+				try {
+					BigDecimal debit = new BigDecimal(tx.getDebit());
+					ft.setDebit(debit);
+				} catch (NumberFormatException ex) {
+					System.out.println("Could not parse debit: " + tx.getDebit());
+				}
+			}
+
+			if (StringUtils.isNotBlank(tx.getCredit())) {
+				try {
+					BigDecimal credit = new BigDecimal(tx.getCredit());
+					ft.setCredit(credit);
+				} catch (NumberFormatException ex) {
+					System.out.println("Could not parse credit: " + tx.getCredit());
+				}
+			}
+
+			ft.setStatus(tx.getStatus());
+			transactions.add(ft);
+		}
+		return transactions;
+	}
+
+	public String categories(Model model) {
+		return "categories";
+	}
+
 	public record TransactionPair(String original, String modified) {
 	}
 
