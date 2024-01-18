4b4501381152356c4f55e9cd468b4278e8e0d386
Aaron Isaac <aaron@aisaac.co>
2024-01-18 00:32:38 -0800

    Add analytics package

---
+++ src/main/java/co/aisaac/finances/analytics/AnalyticsController.java
@@ -0,0 +1,27 @@
+package co.aisaac.finances.analytics;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class AnalyticsController {
+
+	@GetMapping("/analytics")
+	public String analytics() {
+		return "analytics";
+	}
+
+	/*
+	features
+	Main page, show list of accounts on right, click on list to see details for that account
+	Want to see total cash in all accounts
+	Total debt and loans
+	Any crypto or stock purchases
+	Can click on a transaction to see details, or maybe hover
+
+	Set some financial goals
+	Spending by category over month or time period
+
+
+	 */
+}
